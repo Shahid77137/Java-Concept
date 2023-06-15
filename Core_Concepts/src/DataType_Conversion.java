01cbab610b9ea83683fd3497009ebd5b09ebc109
Shahid Afridi <siddiquibabuddin202@gmail.com>
2023-06-15 11:45:20 +0530

    Java Concept

---
+++ Core_Concepts/src/DataType_Conversion.java
@@ -0,0 +1,24 @@
+import java.util.regex.Pattern;
+
+public class DataType_Conversion {
+    public static void main(String[] args) {
+
+        int value1 = 44;
+        long value2 = (long)value1;
+        System.out.println(value2);
+
+        float value3 = 44.56f;
+        double value4 = (double)value3;
+        long value5 = (long)value4;
+        System.out.println(value4);
+        System.out.println(value5);
+
+
+        double a = 7/22*(3.4+2)*3/5;
+        System.out.println(a);
+
+
+
+
+        }
+    }
