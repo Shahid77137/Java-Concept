206dab87168edb81dce4d76c795fd5e9798abba0
Shahid Afridi <siddiquibabuddin202@gmail.com>
2023-08-06 11:45:20 +0530

    Java JPA

---
+++ bootjpa2/src/main/java/com/masai/demo/dao/AlienRepo.java
@@ -0,0 +1,19 @@
+package com.masai.demo.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.masai.demo.model.Alien;
+
+import java.util.List;
+
+public interface AlienRepo extends CrudRepository<Alien,Integer>
+{
+List<Alien> findByTech(String tech);
+
+List<Alien> findByAidGreaterThan(int aid);
+
+@Query("from Alien where tech=?1 order by aname")
+List<Alien>findByTechSorted(String tech);
+
+}
