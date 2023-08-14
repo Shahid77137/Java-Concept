275f20d943eba8822b6a6043cc41b7518d83a60a
Shahid Afridi <siddiquibabuddin202@gmail.com>
2023-08-14 11:45:20 +0530

    Backend with Sping Boot

---
+++ MyWebApp1/src/main/java/com/masai/demo/HomeController.java
@@ -0,0 +1,18 @@
+package com.masai.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController
+{
+	@GetMapping("home")
+	public String home()
+	{
+		System.out.println("hi");
+		return "home";
+	}
+}
