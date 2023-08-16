27042143de7bc30134034ff50e62edddb7a5b550
Shahid Afridi <siddiquibabuddin202@gmail.com>
2023-08-16 16:12:49 +0530

    Inheritance Concept

---
+++ InheritanceExample/src/com/masai/Inheritance/InheritanceConcepts.java
@@ -0,0 +1,16 @@
+package com.masai.Inheritance;
+
+public class InheritanceConcepts {
+/**
+ * If we want a mechanism in which what ever code I am writing in one class(super class) it should 
+ * be available for the some other classes also without writing any extra syntax or anything else 
+ * here concept of inheritance comes into the picture.  
+ * Inheritance is a mechanism by which one object acquire the property of of another object.
+ * What ever be written in the superType is automatically applicable for subType such that no need to write down again
+ * Inheritance provides re-usability
+ * if we adding or removing some operations from the superclass then that operation will be added or removed from the subclasses as well.
+ * In Inheritance whatever you are written in the superclass is available for all subclasses but if whatever you are writing in 
+ * the subclasses it will not available in superclass so vice versa is not true. Inheritance is completely unidirectional
+ * In java Programming single and hirerchial level of inheritance is supported. But multiple Inheritance is not possible or supported.
+**/
+}
---
+++ InheritanceExample/src/com/masai/Inheritance/InheritanceExample.java
@@ -0,0 +1,56 @@
+package com.masai.Inheritance;
+
+//Superclass (Base class)
+class Animal {
+ String name;
+
+ Animal(String name) {
+     this.name = name;
+ }
+
+ void eat() {
+     System.out.println(name + " is eating.");
+ }
+
+ void sleep() {
+     System.out.println(name + " is sleeping.");
+ }
+}
+
+//Subclass (Derived class)
+class Dog extends Animal {
+ Dog(String name) {
+     super(name); // Call the constructor of the superclass
+ }
+
+ void bark() {
+     System.out.println(name + " is barking.");
+ }
+}
+
+//Subclass (Derived class)
+class Cat extends Animal {
+ Cat(String name) {
+     super(name); // Call the constructor of the superclass
+ }
+
+ void meow() {
+     System.out.println(name + " is meowing.");
+ }
+}
+
+public class InheritanceExample {
+ public static void main(String[] args) {
+     Dog dog = new Dog("Buddy");
+     Cat cat = new Cat("Whiskers");
+
+     dog.eat();
+     dog.sleep();
+     dog.bark();
+
+     cat.eat();
+     cat.sleep();
+     cat.meow();
+ }
+}
+
