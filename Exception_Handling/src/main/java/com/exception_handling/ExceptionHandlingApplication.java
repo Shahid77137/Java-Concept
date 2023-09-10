ad73613a0cd0f60bd1f71874354fc9cc1c85c5cf
Shahid Afridi <siddiquibabuddin202@gmail.com>
2023-09-10 12:17:20 +0530

    Java Code

---
+++ Exception_Handling/src/main/java/com/exception_handling/ExceptionHandlingApplication.java
@@ -0,0 +1,14 @@
+package com.exception_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class ExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlingApplication.class, args);
+	}
+
+}
